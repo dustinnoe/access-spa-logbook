3d6994ff820369705208768078a829cb0d9a8f28
dustinnoe <noe.dustin@gmail.com>
2016-03-14 05:24:50 -0500

    Initial commit.  Project wireframe.

---
+++ app/App.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import {Router, Route, browserHistory, IndexRoute} from 'react-router'
+
+class App extends React.Component{
+
+    constructor(props){
+        super(props);
+        this.state = {
+            entries: [],
+            user: null,
+            loading: true
+        };
+
+    }
+    componentDidMount(){
+        this.myDB = new ACCESSdb("C:\\Users\\root\\WebStormProjects\\access-spa-logbook\\database\\logbook-client.mdb", {showErrors:true});
+    }
+    render(){
+        return (
+            <div id="content">
+                    {this.props.children}
+            </div>
+        )
+    }
+}
+
+ReactDOM.render((
+    <Router history={browserHistory}>
+    <Route path="/" component={App}>
+    </Route>
+    </Router>
+), document.getElementById('app'));
